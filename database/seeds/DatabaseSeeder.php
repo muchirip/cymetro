fd21eec61f0bb9e4c65ce3842cf0485ee33d26ee
vagrant <vagrant@homestead>
2020-03-19 15:34:06 +0000

    cymetro database seeding - Events and Reservations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,65 @@
-<?php
+<?php 
 
+// use Illuminate\Database\Seeder;
+
+// class DatabaseSeeder extends Seeder
+// {
+//     /**
+//      * Seed the application's database.
+//      *
+//      * @return void
+//      */
+//     public function run()
+//     {
+        // $this->call(UsersTableSeeder::class);
+    // }
+// }
+
+
+
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-    }
-}
+        $this->call(EventsTableSeeder::class);
+        $this->call(ReservationsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(VansTableSeeder::class);
+        
+    };
+    
+//     public function run()
+// {
+//     factory(App\User::class, 50)->create()->each(function($u) {
+//         $u->posts()->save(factory(App\Post::class)->make());
+    
+//     factory(App\Event::class, 50)->create()->each(function($u) {
+//         $u->posts()->save(factory(App\Post::class)->make());
+
+//     factory(App\Reservation::class, 50)->create()->each(function($u) {
+//         $u->posts()->save(factory(App\Post::class)->make());
+    
+//     factory(App\Van::class, 50)->create()->each(function($u) {
+//         $u->posts()->save(factory(App\Post::class)->make());
+//     });
+    // public function run()
+    // {
+    //     DB::table('users')->insert([
+    //         'first_name' => Str::random(10),
+    //         'last_name' => Str::random(10),
+    //         'email' => Str::random(10).'@gmail.com',
+    //         'isdriver' => integerValue::random(1),
+    //         'password' => bcrypt('password'),
+    //     ]);
+  
+    
+    
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
